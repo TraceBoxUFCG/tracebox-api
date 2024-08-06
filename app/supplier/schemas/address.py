0f681ee4c54e0f8bd4e34c5f9940e388678625c9
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-06 00:55:55 -0300

    feat: zipcode validation

--- app/supplier/schemas/address.py
+++ app/supplier/schemas/address.py
@@ -4,17 +4,21 @@ from uuid import UUID
 from pydantic import BaseModel
 
 from app.common.schemas.states import StatesEnum
+from app.common.schemas.zipcode import ZipCode
 
 
 class AddressBase(BaseModel):
     city: Optional[str] = None
     state: Optional[StatesEnum] = None
     street: Optional[str] = None
-    zipcode: Optional[str] = None
+    zipcode: Optional[ZipCode] = None
     number: Optional[str] = None
     complement: Optional[str] = None
     latitude: float
     longitude: float
+
+    class Config:
+        validate_assignment = True
 
 
 class AddressCreate(AddressBase):
