0c1f350a7774a343923c210217a022712ba782d1
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-07 16:55:59 -0300

    feat: add fastapi pagiation dependency

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 from app.common.exceptions import RecordNotFoundException
 from app.common.exceptions.handler import record_not_found_handler
 from app.common.routes.routes import router
@@ -7,6 +8,7 @@ app = FastAPI()
 
 app.include_router(router=router)
 app.add_exception_handler(RecordNotFoundException, record_not_found_handler)
+add_pagination(app)
 
 
 @app.get("/")
