b3288935fce9ba6114f66fdc2027c3adf3d6444c
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-12 07:02:58 -0300

    feat: config cors

--- app/main.py
+++ app/main.py
@@ -3,12 +3,23 @@ from fastapi_pagination import add_pagination
 from app.common.exceptions import RecordNotFoundException
 from app.common.exceptions.handler import record_not_found_handler
 from app.common.routes.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(router=router)
 app.add_exception_handler(RecordNotFoundException, record_not_found_handler)
 add_pagination(app)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
