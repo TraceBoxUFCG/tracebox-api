8fd372cab2cc926b6f92e27b316b76d2a1fc66bc
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-12 19:11:07 -0300

    feat: add delete supplier

--- app/supplier/routes/supplier.py
+++ app/supplier/routes/supplier.py
@@ -27,3 +27,8 @@ def get_supplier_by_id(
     id: int, service: SupplierService = Depends(get_supplier_service)
 ):
     return service.get_by_id(id=id)
+
+
+@router.delete("/{id}", response_model=Supplier)
+def delete_supplier(id: int, service: SupplierService = Depends(get_supplier_service)):
+    return service.delete(id=id)
