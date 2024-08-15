12d6eb8e0232989534c55a8c2cfea944a0625bdb
Antonio Bertino de Vasconcelos Cabral Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-14 22:13:00 -0300

    feat: endpoint to filter all assets (#14)

--- app/stock/repositories/asset.py
+++ app/stock/repositories/asset.py
@@ -1,17 +1,46 @@
 from uuid import UUID
 
+from sqlalchemy import cast
+import sqlalchemy
 from sqlalchemy.orm import Session
 
-from app.common.repositories.base import BaseRepository
+from app.common.repositories.base import BaseFinder, BaseRepository
 from app.stock.models.asset import AssetModel
+
+
+class AssetFinder(BaseFinder[AssetModel]):
+    @classmethod
+    def get_instance(cls, db: Session):
+        return cls(db.query(AssetModel).filter(AssetModel.deleted_at.is_(None)))
+
+    def filtered_by_id_ilike(self, target: str):
+        if target:
+            return AssetFinder(
+                (
+                    self.base_query.filter(
+                        cast(AssetModel.id, sqlalchemy.String).ilike(
+                            f"%{target.strip()}%"
+                        )
+                    )
+                )
+            )
+        return self
+
+    def search_by_query_criterias(self, target: str):
+        id_query = self.filtered_by_id_ilike(target=target).query
+
+        return AssetFinder(id_query)
 
 
 class AssetRepository(
     BaseRepository[AssetModel, UUID],
 ):
+    finder: AssetFinder
+
     def __init__(self, db: Session):
         super().__init__(
             AssetModel.id,
             model_class=AssetModel,
+            finder=AssetFinder,
             db=db,
         )
--- app/stock/routes/asset.py
+++ app/stock/routes/asset.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
+from fastapi_pagination import Page
 
-from app.stock.schemas.asset import AssetGeneratePayload
+from app.stock.schemas.asset import Asset, AssetGeneratePayload, AssetListParams
 from app.stock.services.asset import AssetService
 from app.supplier.deps import get_asset_service
+from fastapi_pagination.ext.sqlalchemy import paginate
 
 router = APIRouter()
 
@@ -30,3 +32,11 @@ def get_asset_tag(id: int, service: AssetService = Depends(get_asset_service)):
         media_type="application/pdf",
         headers={"Content-Disposition": "attachment;filename=qrcodes.pdf"},
     )
+
+
+@router.get("/", response_model=Page[Asset])
+def get_all_tags(
+    params: AssetListParams = Depends(),
+    service: AssetService = Depends(get_asset_service),
+):
+    return paginate(service.get_all_for_pagination(params=params))
